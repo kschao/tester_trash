61222716f36d00a975ec284f6d7614eb0fb2a924
Dustin Koy <dustin.koy@Dustins-MacBook-Pro.local>
2021-08-04 18:55:04 -0500

    code in customer/urls.py for added path

--- trash_collector/customers/urls.py
+++ trash_collector/customers/urls.py
@@ -7,4 +7,7 @@ from . import views
 app_name = "customers"
 urlpatterns = [
     path('', views.index, name="index"),
+    path('new/', views.create, name='create'),
+    path('change/', views.change, name='change'),
+    path('account/', views.account, name='account')
 ]
