c903302692ee5b8782f15bb6703f135c23b481d6
Dustin Koy <dustin.koy@Dustins-MacBook-Pro.local>
2021-08-05 09:04:27 -0500

    def index and create in views.py employees

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from .models import Employees
+from django.shortcuts import reverse
+from datetime import datetime as dt
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
 
@@ -8,6 +11,37 @@ from django.apps import apps
 
 
 def index(request):
+    user = request.user
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    customers = Customer.objects.all()
+    now = dt.now
+    today = now.strftime('%A')
+    try:
+        log_in_employee = Employees.objects.get(user=user)
+    except: return HttpResponseRedirect(reverse('employees:create'))
+    occurring_customers = []
+    for customer in customers:
+        if customer.zipcode == log_in_employee.zipcode:
+            occurring_customers.append(customer.name)
+    context = {
+        'log_in_employee': log_in_employee,
+        'customers': occurring_customers,
+        'customers_address': customer.address
+        }
+    print(occurring_customers)
+    return render(request, 'employees/index.html', context)
+
+def create(request):
+    if request.method == "POST":
+        user = request.user
+        name = request.POST.get('name')
+        zipcode = request.POST.get('zipcode')
+        new_employee = Employees(name=name, user=user, zipcode=zipcode)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees/create.html'))
+    else:
+        return render(request, 'employees/create.html')
+
+    
+
